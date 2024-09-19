2d764854cc50450784b377ddfaa395f501bf31a6
saravnansoftdev <saravanan.softdev@gmail.com>
2024-09-19 06:47:08 +0530

    .

---
+++ leapYear.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the year: ");
+    scanf("%d",&n);
+    if(n%4==0)
+    {
+        printf("%d is a leap year",n);
+    }
+    else
+    {
+        printf("%d is not a leap year",n);
+    }
+    return;
+}
